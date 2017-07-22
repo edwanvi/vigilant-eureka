480fd0be9a8ce690478caaf7e650b4351e28dc5b
Edwan Vi <edwan.vi@gmail.com>
2017-07-22 00:26:41 -0400

    Add creative tab

--- src/main/java/me/itstheholyblack/vigilant_eureka/items/ModItems.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/items/ModItems.java
@@ -1,6 +1,8 @@
 package me.itstheholyblack.vigilant_eureka.items;
 
 import me.itstheholyblack.vigilant_eureka.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -8,6 +10,13 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ModItems {
     @GameRegistry.ObjectHolder(Reference.MOD_ID + ":dim_key")
     public static DimKey dimKey;
+
+    public static final CreativeTabs CREATIVETAB = new CreativeTabs("vigilantEureka") {
+        @Override
+        public ItemStack getTabIconItem() {
+            return new ItemStack(dimKey);
+        }
+    };
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
