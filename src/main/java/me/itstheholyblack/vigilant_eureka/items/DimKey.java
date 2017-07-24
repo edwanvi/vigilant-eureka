d15ff7e3faa6afa65afc9cf91f8f223dd375e426
Edwan Vi <edwan.vi@gmail.com>
2017-07-23 21:29:59 -0400

    Remove unused imports, bump version

--- src/main/java/me/itstheholyblack/vigilant_eureka/items/DimKey.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/items/DimKey.java
@@ -16,8 +16,6 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
