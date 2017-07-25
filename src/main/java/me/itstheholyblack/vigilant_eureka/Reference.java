3bad1ae4ce9dbaacd82750d738d1b443b23575e5
Edwan Vi <edwan.vi@gmail.com>
2017-07-25 11:41:01 -0400

    Internal version bump

--- src/main/java/me/itstheholyblack/vigilant_eureka/Reference.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/Reference.java
@@ -3,4 +3,5 @@ package me.itstheholyblack.vigilant_eureka;
 public class Reference {
     public static final String MOD_NAME = "Vigilant Eureka";
     public static final String MOD_ID = "vigilant_eureka";
+    public static final String VERSION = "0.0.1-alpha";
 }
