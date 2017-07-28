9ecf74234681e0dbb07f5ce962bffed3f52dc8f9
Edwan Vi <edwan.vi@gmail.com>
2017-07-28 16:22:32 -0400

    Finish rename + add texture to item

--- src/main/java/me/itstheholyblack/vigilant_eureka/items/ModItems.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/items/ModItems.java
@@ -1,7 +1,7 @@
 package me.itstheholyblack.vigilant_eureka.items;
 
 import me.itstheholyblack.vigilant_eureka.Reference;
-import me.itstheholyblack.vigilant_eureka.items.armor.WarpFeets;
+import me.itstheholyblack.vigilant_eureka.items.armor.WarpBoots;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -11,8 +11,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ModItems {
     @GameRegistry.ObjectHolder(Reference.MOD_ID + ":dim_key")
     public static DimKey dimKey;
-    @GameRegistry.ObjectHolder(Reference.MOD_ID + ":warp_feets")
-    public  static WarpFeets warpFeets;
+    @GameRegistry.ObjectHolder(Reference.MOD_ID + ":warp_boots")
+    public static WarpBoots warpBoots;
 
     public static final CreativeTabs CREATIVETAB = new CreativeTabs("vigilantEureka") {
         @Override
@@ -24,5 +24,6 @@ public class ModItems {
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         dimKey.initModel();
+        warpBoots.initModel();
     }
 }
--- src/main/java/me/itstheholyblack/vigilant_eureka/items/armor/WarpBoots.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/items/armor/WarpBoots.java
@@ -3,36 +3,46 @@ package me.itstheholyblack.vigilant_eureka.items.armor;
 import me.itstheholyblack.vigilant_eureka.Reference;
 import me.itstheholyblack.vigilant_eureka.items.ModItems;
 import me.itstheholyblack.vigilant_eureka.util.RayTraceHelper;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class WarpFeets extends ItemArmor {
-    public WarpFeets() {
+public class WarpBoots extends ItemArmor {
+    public WarpBoots() {
         super(ArmorMaterial.CHAIN, 0, EntityEquipmentSlot.FEET);
-        setUnlocalizedName(Reference.MOD_ID + "." + "warp_feets");
-        setRegistryName("warp_feets");
+        setUnlocalizedName(Reference.MOD_ID + "." + "warp_boots");
+        setRegistryName("warp_boots");
         setCreativeTab(ModItems.CREATIVETAB);
     }
 
     @Override
     public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
-        if (player.isSprinting()) {
+        if (!world.isRemote && player.isSprinting()) {
             BlockPos look;
             try {
-                look = RayTraceHelper.tracePath(world, player, 10, 1, null).getBlockPos();
+                look = RayTraceHelper.tracePath(world, player, 25, 1, null).getBlockPos();
             } catch (NullPointerException e) {
                 look = player.getPosition();
             }
-            if (look.equals(player.getPosition())) {
+            if (look.equals(player.getPosition()) || player.getCooldownTracker().hasCooldown(this)) {
                 return;
             } else {
                 player.setSprinting(false);
+                player.getCooldownTracker().setCooldown(this, 20);
                 player.setPositionAndUpdate(look.getX(), look.up().getY(), look.getZ());
             }
         }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 }
