acbed9b9ab077b009361101ae887eb7b2bee76a1
Edwan Vi <edwan.vi@gmail.com>
2017-07-28 18:25:13 -0400

    More JAR signing

--- src/main/java/me/itstheholyblack/vigilant_eureka/Reference.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/Reference.java
@@ -4,4 +4,5 @@ public class Reference {
     public static final String MOD_NAME = "Vigilant Eureka";
     public static final String MOD_ID = "vigilant_eureka";
     public static final String VERSION = "0.0.1-alpha";
+    public static final String MOD_KEY = "19d2daed26722f2762d067603604a6d1f909f262";
 }
