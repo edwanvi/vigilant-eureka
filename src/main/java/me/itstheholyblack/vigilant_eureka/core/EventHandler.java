7c6d2c4d2231848252d9cb2af44e1905a02c95fd
Edwan Vi <edwan.vi@gmail.com>
2017-07-29 13:09:33 -0400

    Remove unused imports

--- src/main/java/me/itstheholyblack/vigilant_eureka/core/EventHandler.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/core/EventHandler.java
@@ -1,29 +1,20 @@
 package me.itstheholyblack.vigilant_eureka.core;
 
-import com.google.common.base.Predicates;
 import me.itstheholyblack.vigilant_eureka.Reference;
 import me.itstheholyblack.vigilant_eureka.items.ModItems;
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.RenderLivingBase;
-import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
-
-import java.util.List;
 
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID)
 public class EventHandler {
@@ -61,6 +52,7 @@ public class EventHandler {
             }
         }
     }
+
     @SubscribeEvent
     public void armorChanged(LivingEquipmentChangeEvent event) {
         ItemStack oldStack = event.getFrom();
