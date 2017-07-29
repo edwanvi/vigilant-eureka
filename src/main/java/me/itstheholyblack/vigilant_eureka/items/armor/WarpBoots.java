3f584db664edd5b3ff445c43a5db3f5fe1b55dcd
Edwan Vi <edwan.vi@gmail.com>
2017-07-29 13:16:14 -0400

    Add mouseover text to warp boots.

--- src/main/java/me/itstheholyblack/vigilant_eureka/items/armor/WarpBoots.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/items/armor/WarpBoots.java
@@ -4,6 +4,8 @@ import me.itstheholyblack.vigilant_eureka.Reference;
 import me.itstheholyblack.vigilant_eureka.items.ModItems;
 import me.itstheholyblack.vigilant_eureka.util.RayTraceHelper;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
@@ -13,6 +15,8 @@ import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 public class WarpBoots extends ItemArmor {
     public WarpBoots() {
@@ -44,5 +48,11 @@ public class WarpBoots extends ItemArmor {
     @SideOnly(Side.CLIENT)
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(I18n.format("mouseovertext.warp_boots"));
     }
 }
