59c4d8a0afe7f8389475f27a94155230e33810ab
Edwan Vi <edwan.vi@gmail.com>
2017-07-30 01:30:54 -0400

    Remove warp code from boots (will be handled by server on packet)

--- src/main/java/me/itstheholyblack/vigilant_eureka/items/armor/WarpBoots.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/items/armor/WarpBoots.java
@@ -2,15 +2,12 @@ package me.itstheholyblack.vigilant_eureka.items.armor;
 
 import me.itstheholyblack.vigilant_eureka.Reference;
 import me.itstheholyblack.vigilant_eureka.items.ModItems;
-import me.itstheholyblack.vigilant_eureka.util.RayTraceHelper;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
@@ -24,25 +21,6 @@ public class WarpBoots extends ItemArmor {
         setUnlocalizedName(Reference.MOD_ID + "." + "warp_boots");
         setRegistryName("warp_boots");
         setCreativeTab(ModItems.CREATIVETAB);
-    }
-
-    @Override
-    public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
-        if (!world.isRemote && player.isSprinting()) {
-            BlockPos look;
-            try {
-                look = RayTraceHelper.tracePath(world, player, 25, 1, null).getBlockPos();
-            } catch (NullPointerException e) {
-                look = player.getPosition();
-            }
-            if (look.equals(player.getPosition()) || player.getCooldownTracker().hasCooldown(this)) {
-                return;
-            } else {
-                player.setSprinting(false);
-                player.getCooldownTracker().setCooldown(this, 20);
-                player.setPositionAndUpdate(look.getX(), look.up().getY(), look.getZ());
-            }
-        }
     }
 
     @SideOnly(Side.CLIENT)
