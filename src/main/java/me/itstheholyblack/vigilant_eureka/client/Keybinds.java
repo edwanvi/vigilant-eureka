3a84028be7ae68ebdc12cc01082bbe3035e212a1
Edwan Vi <edwan.vi@gmail.com>
2017-07-30 01:30:04 -0400

    Add and localize keybind for boots

---
+++ src/main/java/me/itstheholyblack/vigilant_eureka/client/Keybinds.java
@@ -0,0 +1,24 @@
+package me.itstheholyblack.vigilant_eureka.client;
+
+import me.itstheholyblack.vigilant_eureka.Reference;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.input.Keyboard;
+
+@SideOnly(Side.CLIENT)
+public class Keybinds {
+
+    public static KeyBinding warpKey;
+
+    private static KeyBinding init(String s, int key, String group) {
+        KeyBinding kb = new KeyBinding(Reference.MOD_ID + ".keybind." + s, key, group);
+        ClientRegistry.registerKeyBinding(kb);
+        return kb;
+    }
+
+    public static void initWarpKey() {
+        warpKey = init("warp_key", Keyboard.KEY_C, Reference.MOD_ID + ".gui.keybinds");
+    }
+}
