3a40720c4667d71defffc27624db0fcf94623817
Edwan Vi <edwan.vi@gmail.com>
2017-07-31 17:24:45 -0400

    No idea where this rabbit hole is going, but I like it

--- src/main/java/me/itstheholyblack/vigilant_eureka/blocks/tiles/LeyLineTile.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/blocks/tiles/LeyLineTile.java
@@ -1,12 +1,16 @@
 package me.itstheholyblack.vigilant_eureka.blocks.tiles;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTUtil;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class LeyLineTile extends TileEntity implements ITickable {
@@ -14,7 +18,7 @@ public class LeyLineTile extends TileEntity implements ITickable {
     private UUID name;
     public float ticks = 0;
 
-    // private ArrayList<BlockPos> links;
+    private ArrayList<BlockPos> links;
 
     public LeyLineTile() {
         super();
@@ -30,13 +34,21 @@ public class LeyLineTile extends TileEntity implements ITickable {
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
         this.name = compound.getUniqueId("name");
-        // this.links = compound.getByteArray("links").toString();
+        NBTTagList n = (NBTTagList) compound.getTag("links");
+        for (NBTBase tag : n) {
+            this.links.add(NBTUtil.getPosFromTag((NBTTagCompound) tag));
+        }
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         super.writeToNBT(compound);
         compound.setUniqueId("name", this.name);
+        NBTTagList n = new NBTTagList();
+        for (BlockPos p : links) {
+            n.appendTag(NBTUtil.createPosTag(p));
+        }
+        compound.setTag("links", n);
         return compound;
     }
 
