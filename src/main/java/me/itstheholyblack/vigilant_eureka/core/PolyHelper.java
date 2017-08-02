7c40ea7c5073a8e1f83d858c895e0b9f0df6e0a5
Edwan Vi <edwan.vi@gmail.com>
2017-08-02 09:03:24 -0400

    Copy from StackOverflow
    https://stackoverflow.com/questions/8721406/how-to-determine-if-a-point-is-inside-a-2d-convex-polygon#8721483

--- src/main/java/me/itstheholyblack/vigilant_eureka/core/PolyHelper.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/core/PolyHelper.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
-public class PolySolver {
+public class PolyHelper {
     public static ArrayList<BlockPos> stackSolve(LeyLineTile te) {
         ArrayList<BlockPos> polygon = new ArrayList<>();
         Stack<BlockPos> stack = new Stack<>();
@@ -31,5 +31,25 @@ public class PolySolver {
             t = (LeyLineTile) t.getWorld().getTileEntity(t.getLinkOut());
         }
         return null;
+    }
+
+    /**
+     * Return true if the given point is contained inside the boundary.
+     * See: http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+     *
+     * @param test The point to check
+     * @return true if the point is inside the boundary, false otherwise
+     */
+    public static boolean contains(BlockPos test, ArrayList<BlockPos> points) {
+        int i;
+        int j;
+        boolean result = false;
+        for (i = 0, j = points.size() - 1; i < points.size(); j = i++) {
+            if ((points.get(i).getZ() > test.getZ()) != (points.get(j).getZ() > test.getZ()) &&
+                    (test.getX() < (points.get(j).getX() - points.get(i).getX()) * (test.getZ() - points.get(i).getZ()) / (points.get(j).getZ() - points.get(i).getZ()) + points.get(i).getX())) {
+                result = !result;
+            }
+        }
+        return result;
     }
 }
