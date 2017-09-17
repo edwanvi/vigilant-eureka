0f0f808e47d275d0139cbe8cc7cb53f3ec29969c
Edwan Vi <edwan.vi@gmail.com>
2017-09-16 22:45:12 -0400

    Add more structures

--- src/main/java/me/itstheholyblack/vigilant_eureka/world/WorldGenLey.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/world/WorldGenLey.java
@@ -22,41 +22,58 @@ public class WorldGenLey implements IWorldGenerator {
 
     private ArrayList<ResourceLocation> resourceLocations = new ArrayList<>();
 
+    private static ResourceLocation LEY_STRUCT_ABS_1 = new ResourceLocation(Reference.MOD_ID + ":leystruct1");
+    private static ResourceLocation LAYING_STATUE = new ResourceLocation(Reference.MOD_ID + ":layingley");
+    private static ResourceLocation LEY_WELL = new ResourceLocation(Reference.MOD_ID + ":leywell");
+    private static ResourceLocation LEY_STATUE_1 = new ResourceLocation(Reference.MOD_ID + ":leystatue1");
+    private static ResourceLocation LEY_LIBRARY = new ResourceLocation(Reference.MOD_ID + ":leylibrary");
+
     public WorldGenLey() {
-        resourceLocations.add(new ResourceLocation(Reference.MOD_ID + ":leystruct1"));
-        resourceLocations.add(new ResourceLocation(Reference.MOD_ID + ":layingley"));
+        resourceLocations.add(LEY_STRUCT_ABS_1);
+        resourceLocations.add(LAYING_STATUE);
+        resourceLocations.add(LEY_WELL);
+        resourceLocations.add(LEY_STATUE_1);
+        resourceLocations.add(LEY_LIBRARY);
     }
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
         if (!world.isRemote) {
-            // world randomizer instance
-            Random rand = world.rand;
-            if (rand.nextInt(100) < 25) {
+            if (random.nextInt(200) < 1) {
                 // get values for template
                 WorldServer server = (WorldServer) world;
                 TemplateManager templateManager = server.getStructureTemplateManager();
-                ResourceLocation r = resourceLocations.get(world.rand.nextInt(resourceLocations.size()));
+                ResourceLocation r = resourceLocations.get(random.nextInt(resourceLocations.size()));
                 // fetch template
                 Template t = templateManager.get(world.getMinecraftServer(), r);
                 // actual block level co-ords of chunk
                 int blockX = chunkX * 16;
                 int blockZ = chunkZ * 16;
                 // get block level generation position
-                int XModifier = rand.nextInt(16);
-                int ZModifier = rand.nextInt(16);
+                int XModifier = random.nextInt(16);
+                int ZModifier = random.nextInt(16);
                 while (!(XModifier == 0 || XModifier == 15 || ZModifier == 0 || ZModifier == 15)) {
-                    XModifier = rand.nextInt(16);
-                    ZModifier = rand.nextInt(16);
+                    XModifier = random.nextInt(16);
+                    ZModifier = random.nextInt(16);
                 }
                 int randX = blockX + XModifier;
                 int randZ = blockZ + ZModifier;
                 // get ground level + 1 at (randX, randZ)
-                int y = 1 + getGroundFromAbove(world, randX, randZ);
+                int y;
+                if (!(r.equals(LEY_WELL) || r.equals(LEY_LIBRARY))) {
+                    y = 1 + getGroundFromAbove(world, randX, randZ);
+                } else {
+                    y = getGroundFromAbove(world, randX, randZ);
+                }
+                if (y < 0) {
+                    System.out.println("Y < 0");
+                    return;
+                }
+                System.out.format("Generating %s at (%d, %d, %d)\n", r.toString(), randX, y, randZ);
                 PlacementSettings settings = new PlacementSettings();
                 settings.setRandom(world.rand);
                 // Get random rotation value
-                settings.setRotation(Rotation.values()[rand.nextInt(Rotation.values().length)]);
+                settings.setRotation(Rotation.values()[random.nextInt(4)]);
                 t.addBlocksToWorld(world, new BlockPos(randX, y, randZ), settings);
             }
         }
@@ -66,12 +83,11 @@ public class WorldGenLey implements IWorldGenerator {
     int getGroundFromAbove(World world, int x, int z) {
         int y = 255;
         boolean foundGround = false;
-        while (!foundGround && y-- >= 0) {
+        while (!foundGround && y-- >= 20) {
             Block blockAt = world.getBlockState(new BlockPos(x, y, z)).getBlock();
             // "ground" is grass, sand, and dirt.
-            foundGround = blockAt.equals(Blocks.DIRT) || blockAt.equals(Blocks.GLASS) || blockAt.equals(Blocks.SAND);
+            foundGround = (blockAt.equals(Blocks.DIRT) || blockAt.equals(Blocks.GRASS) || blockAt.equals(Blocks.SAND));
         }
-
         return y;
     }
 }
