96b9e854c2486f31900127f847b47cb6655e75cd
Edwan Vi <edwan.vi@gmail.com>
2018-05-12 14:48:48 -0400

    Rename PacketSendKey -> PacketSendWarp

--- src/main/java/me/itstheholyblack/vigilant_eureka/network/PacketSendWarp.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/network/PacketSendWarp.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-public class PacketSendKey implements IMessage {
+public class PacketSendWarp implements IMessage {
     private BlockPos blockPos;
 
     @Override
@@ -28,15 +28,15 @@ public class PacketSendKey implements IMessage {
         buf.writeLong(blockPos.toLong());
     }
 
-    public PacketSendKey() {
+    public PacketSendWarp() {
         // Calculate the position of the block we are looking at
         RayTraceResult result = Minecraft.getMinecraft().objectMouseOver;
         blockPos = result.getBlockPos();
     }
 
-    public static class Handler implements IMessageHandler<PacketSendKey, IMessage> {
+    public static class Handler implements IMessageHandler<PacketSendWarp, IMessage> {
         @Override
-        public IMessage onMessage(PacketSendKey message, MessageContext ctx) {
+        public IMessage onMessage(PacketSendWarp message, MessageContext ctx) {
             // Always use a construct like this to actually handle your message. This ensures that
             // your 'handle' code is run on the main Minecraft thread. 'onMessage' itself
             // is called on the networking thread so it is not safe to do a lot of things
@@ -45,7 +45,7 @@ public class PacketSendKey implements IMessage {
             return null;
         }
 
-        private void handle(PacketSendKey message, MessageContext ctx) {
+        private void handle(PacketSendWarp message, MessageContext ctx) {
             // This code is run on the server side. So you can do server-side calculations here
             EntityPlayerMP playerEntity = ctx.getServerHandler().player;
             World world = playerEntity.getEntityWorld();
