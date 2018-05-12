1018ff38763c55cdab9c26baaeb97a99947d0845
Edwan Vi <edwan.vi@gmail.com>
2018-05-12 11:22:34 -0400

    Let's try that again.

--- src/main/java/me/itstheholyblack/vigilant_eureka/items/DebugStick.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/items/DebugStick.java
@@ -1,6 +1,7 @@
 package me.itstheholyblack.vigilant_eureka.items;
 
 import me.itstheholyblack.vigilant_eureka.Reference;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
@@ -8,13 +9,13 @@ import net.minecraft.item.Item;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
-import net.minecraft.world.gen.structure.template.PlacementSettings;
-import net.minecraft.world.gen.structure.template.Template;
-import net.minecraft.world.gen.structure.template.TemplateManager;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -32,11 +33,29 @@ public class DebugStick extends Item {
 
     @Override
     public EnumActionResult onItemUse(EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (!worldIn.isRemote) {
-            WorldServer server = (WorldServer) worldIn;
-            TemplateManager templateManager = server.getStructureTemplateManager();
-            Template template = templateManager.get(null, new ResourceLocation(Reference.MOD_ID + ":leystruct1"));
-            template.addBlocksToWorld(worldIn, pos, new PlacementSettings());
+        if (!worldIn.isRemote && worldIn instanceof WorldServer) {
+            try {
+                Chunk oldChunk = worldIn.getChunkFromChunkCoords(pos.getX() >> 4, pos.getZ() >> 4);
+                IChunkProvider provider = worldIn.getChunkProvider();
+                IChunkGenerator generator = ((ChunkProviderServer) provider).chunkGenerator;
+                Chunk newChunk = generator.generateChunk(oldChunk.x, oldChunk.z);
+
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        for (int y = 0; y < worldIn.getHeight(); y++) {
+                            IBlockState state = newChunk.getBlockState(x, y, z);
+                            worldIn.setBlockState(new BlockPos(x + oldChunk.x * 16, y, z + oldChunk.z * 16), state, 3);
+                        }
+                    }
+                }
+
+                oldChunk.setTerrainPopulated(false);
+                generator.populate(oldChunk.x, oldChunk.z);
+                oldChunk.markDirty();
+                oldChunk.resetRelightChecks();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return EnumActionResult.SUCCESS;
     }
