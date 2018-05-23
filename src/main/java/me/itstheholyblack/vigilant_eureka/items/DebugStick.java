fbbf7bd03c0ea1e34aedd2efd7171a02f6b710dc
Edwan Vi <edwan.vi@gmail.com>
2018-05-23 07:15:18 -0400

    Bind spawning to the debug stick for testing

--- src/main/java/me/itstheholyblack/vigilant_eureka/items/DebugStick.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/items/DebugStick.java
@@ -1,12 +1,14 @@
 package me.itstheholyblack.vigilant_eureka.items;
 
 import me.itstheholyblack.vigilant_eureka.Reference;
-import me.itstheholyblack.vigilant_eureka.entity.EntityPlayerBody;
-import net.minecraft.client.Minecraft;
+import me.itstheholyblack.vigilant_eureka.network.PacketHandler;
+import me.itstheholyblack.vigilant_eureka.network.PacketSpawnBody;
+import me.itstheholyblack.vigilant_eureka.util.NBTUtil;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -29,11 +31,15 @@ public class DebugStick extends Item {
 
     @Override
     public EnumActionResult onItemUse(EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        EntityPlayerBody e = new EntityPlayerBody(worldIn);
-        e.setPosition(pos.getX(), pos.getY(), pos.getZ());
-        e.setPlayer(playerIn.getGameProfile().getName());
-        System.out.println(playerIn.getGameProfile().getName());
-        worldIn.spawnEntity(e);
-        return EnumActionResult.SUCCESS;
+        NBTTagCompound persist = NBTUtil.getPlayerPersist(playerIn);
+        if (!persist.getBoolean("metaphysical_high_ground")) {
+            persist.setBoolean("metaphysical_high_ground", true);
+            playerIn.setInvisible(true);
+            if (worldIn.isRemote) {
+                PacketHandler.INSTANCE.sendToServer(new PacketSpawnBody());
+            }
+            return EnumActionResult.SUCCESS;
+        }
+        return EnumActionResult.FAIL;
     }
 }
