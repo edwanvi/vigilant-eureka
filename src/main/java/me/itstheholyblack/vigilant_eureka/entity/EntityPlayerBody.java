6185466f857c2c40bfa29f7ac1add79eb5bf5066
Edwan Vi <edwan.vi@gmail.com>
2018-05-23 07:13:41 -0400

    Create EntityPlayerBody + renderer

--- src/main/java/me/itstheholyblack/vigilant_eureka/entity/EntityPlayerBody.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/entity/EntityPlayerBody.java
@@ -1,12 +1,16 @@
 package me.itstheholyblack.vigilant_eureka.entity;
 
+import me.itstheholyblack.vigilant_eureka.util.NBTUtil;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
 
 public class EntityPlayerBody extends EntityLiving {
 
@@ -15,7 +19,7 @@ public class EntityPlayerBody extends EntityLiving {
 
     public EntityPlayerBody(World worldIn) {
         super(worldIn);
-        setSize(0.6F, 1.8F);
+        setSize(0.6F, 0.5F);
         setHealth(20);
     }
 
@@ -40,11 +44,32 @@ public class EntityPlayerBody extends EntityLiving {
         setSmallArms(compound.getBoolean("smallArms"));
     }
 
+    @Override
+    public boolean attackEntityFrom(@Nonnull DamageSource source, float amount) {
+        EntityPlayer p = this.world.getPlayerEntityByName(this.getPlayer());
+        return (p != null) ? super.attackEntityFrom(source, amount) && p.attackEntityFrom(source, amount) : super.attackEntityFrom(source, amount);
+    }
+
+    @Override
+    public void onDeath(DamageSource cause) {
+        super.onDeath(cause);
+        EntityPlayer p = this.world.getPlayerEntityByName(this.getPlayer());
+        if (p != null) {
+            NBTUtil.getPlayerPersist(p).setBoolean("metaphysical_high_ground", false);
+            p.setInvisible(false);
+        }
+    }
+
     public String getPlayer() {
         return dataManager.get(PLAYER_NAME);
     }
 
     public void setPlayer(String player) {
+        if (player.endsWith("s")) {
+            this.setCustomNameTag(player + "' body");
+        } else {
+            this.setCustomNameTag(player + "'s body");
+        }
         dataManager.set(PLAYER_NAME, player);
     }
 
--- src/main/java/me/itstheholyblack/vigilant_eureka/entity/render/RenderEntityPlayerBody.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/entity/render/RenderEntityPlayerBody.java
@@ -1,17 +1,22 @@
 package me.itstheholyblack.vigilant_eureka.entity.render;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import me.itstheholyblack.vigilant_eureka.entity.EntityPlayerBody;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
 
 public class RenderEntityPlayerBody extends RenderBiped<EntityPlayerBody> {
+    private static final ResourceLocation TEXTURE_STEVE = new ResourceLocation("textures/entity/steve.png");
+    private static final ResourceLocation TEXTURE_ALEX = new ResourceLocation("textures/entity/alex.png");
+
     public RenderEntityPlayerBody(RenderManager renderManagerIn) {
         super(renderManagerIn, new ModelPlayer(0.0F, false), 0);
     }
@@ -22,14 +27,24 @@ public class RenderEntityPlayerBody extends RenderBiped<EntityPlayerBody> {
         super.doRender(dopple, x, y, z, yaw, partialTicks);
     }
 
+    @Override
+    protected void applyRotations(EntityPlayerBody entityLiving, float p_77043_2_, float rotationYaw, float partialTicks) {
+        GlStateManager.rotate(180.0F - rotationYaw, 0, 1, 0);
+        GlStateManager.translate(0, 0.24, 0);
+        GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
+    }
+
     @Nonnull
     @Override
     protected ResourceLocation getEntityTexture(@Nonnull EntityPlayerBody entity) {
-        Minecraft mc = Minecraft.getMinecraft();
+        GameProfile profile = new GameProfile(null, entity.getPlayer());
+        Minecraft minecraft = Minecraft.getMinecraft();
+        Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> map = minecraft.getSkinManager().loadSkinFromCache(profile);
 
-        if (!(mc.getRenderViewEntity() instanceof AbstractClientPlayer))
-            return DefaultPlayerSkin.getDefaultSkinLegacy();
-
-        return ((AbstractClientPlayer) mc.getRenderViewEntity()).getLocationSkin();
+        if (map.containsKey(MinecraftProfileTexture.Type.SKIN)) {
+            return minecraft.getSkinManager().loadSkin(map.get(MinecraftProfileTexture.Type.SKIN), MinecraftProfileTexture.Type.SKIN);
+        } else {
+            return entity.smallArms() ? TEXTURE_ALEX : TEXTURE_STEVE;
+        }
     }
 }
