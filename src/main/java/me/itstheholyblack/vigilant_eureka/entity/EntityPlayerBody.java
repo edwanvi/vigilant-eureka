b8d9e3387bb76532d374dc6d7b2416b101f3c119
Edwan Vi <edwan.vi@gmail.com>
2018-06-18 19:42:58 -0400

    Sync potions between body and player

--- src/main/java/me/itstheholyblack/vigilant_eureka/entity/EntityPlayerBody.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/entity/EntityPlayerBody.java
@@ -10,13 +10,19 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Collection;
 import java.util.UUID;
 
 public class EntityPlayerBody extends EntityLiving {
@@ -24,6 +30,7 @@ public class EntityPlayerBody extends EntityLiving {
     private static final DataParameter<String> PLAYER_USERNAME = EntityDataManager.createKey(EntityPlayerBody.class, DataSerializers.STRING);
     private static final DataParameter<Optional<UUID>> PLAYER_UUID = EntityDataManager.createKey(EntityPlayerBody.class, DataSerializers.OPTIONAL_UNIQUE_ID);
     private static final DataParameter<Boolean> SMALL_ARMS = EntityDataManager.createKey(EntityPlayerBody.class, DataSerializers.BOOLEAN);
+    private static final ITextComponent INTERACT_MESSAGE = new TextComponentTranslation("message.body").setStyle(new Style().setColor(TextFormatting.GOLD));
 
     public EntityPlayerBody(World worldIn) {
         super(worldIn);
@@ -61,13 +68,37 @@ public class EntityPlayerBody extends EntityLiving {
     public void onLivingUpdate() {
         super.onLivingUpdate();
         this.limbSwingAmount = 0.0F;
+        EntityPlayer p = this.world.getPlayerEntityByUUID(this.getPlayerId());
+        if (p != null && !world.isRemote) {
+            // i'm messing with the scoreboard and there's nothing you can do to stop me
+            if (!this.isOnSameTeam(p)) {
+                if (this.getTeam() != null) {
+                    world.getScoreboard().removePlayerFromTeam(this.getCachedUniqueIdString(), (ScorePlayerTeam) this.getTeam());
+                }
+                world.getScoreboard().addPlayerToTeam(this.getCachedUniqueIdString(), p.getTeam().getName());
+            }
+            // "sync" potions
+            Collection<PotionEffect> playerEffects = p.getActivePotionEffects();
+            Collection<PotionEffect> bodyEffects = this.getActivePotionEffects();
+            for (PotionEffect effect : bodyEffects) {
+                if (!playerEffects.contains(effect)) {
+                    this.removeActivePotionEffect(effect.getPotion());
+                    this.markPotionsDirty();
+                }
+            }
+            for (PotionEffect effect : playerEffects) {
+                if (!this.isPotionActive(effect.getPotion())) {
+                    this.addPotionEffect(effect);
+                }
+            }
+        }
     }
 
     @Override
     public boolean processInteract(EntityPlayer player, EnumHand hand) {
         if (player.getName().equals(this.getPlayerName())) {
             if (!player.getHeldItem(hand).isEmpty() && player.getUniqueID().equals(this.getPlayerId())) {
-                player.sendStatusMessage(new TextComponentTranslation("message.body"), true);
+                player.sendStatusMessage(INTERACT_MESSAGE, true);
             } else if (player.getUniqueID().equals(this.getPlayerId())) {
                 this.attackEntityFrom(DamageSource.OUT_OF_WORLD, this.getHealth());
             }
