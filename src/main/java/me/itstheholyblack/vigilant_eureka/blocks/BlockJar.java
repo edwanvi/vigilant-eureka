b16a8d97f3ab45299738190a5d6935ee0315f1aa
Edwan Vi <edwan.vi@gmail.com>
2019-05-11 16:23:59 -0400

    Shrink hitbox

--- src/main/java/me/itstheholyblack/vigilant_eureka/blocks/BlockJar.java
+++ src/main/java/me/itstheholyblack/vigilant_eureka/blocks/BlockJar.java
@@ -12,9 +12,13 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 
@@ -25,6 +29,41 @@ public class BlockJar extends BlockTileEntity<JarTile> {
         setTranslationKey(Reference.MOD_ID + ".vexjar");
         setRegistryName("vexjar");
         setCreativeTab(ModItems.CREATIVE_TAB);
+    }
+
+    /**
+     * @deprecated call via {@link IBlockState#getCollisionBoundingBox(IBlockAccess, BlockPos)} whenever possible.
+     * Implementing/overriding is fine.
+     */
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+        return new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.9375D, 0.9375D);
+    }
+
+    /**
+     * Return an AABB (in world coords!) that should be highlighted when the player is targeting this Block
+     *
+     * @deprecated call via {@link IBlockState#getSelectedBoundingBox(World, BlockPos)} whenever possible.
+     * Implementing/overriding is fine.
+     */
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos) {
+        return new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 1.0D, 0.9375D).offset(pos);
+    }
+
+    /**
+     * @deprecated call via {@link IBlockState#isFullCube()} whenever possible. Implementing/overriding is fine.
+     */
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    /**
+     * Used to determine ambient occlusion and culling when rebuilding chunks for render
+     *
+     * @deprecated call via {@link IBlockState#isOpaqueCube()} whenever possible. Implementing/overriding is fine.
+     */
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
     }
 
     @Override
